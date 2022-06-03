89da04b2cd763a764462c4a8c34309157f420a74
momo3038 <morgan.leroi@gmail.com>
2022-06-03 13:33:21 +0200

    feat(api): Add secret management

--- api/src/algolia/algolia.service.ts
+++ api/src/algolia/algolia.service.ts
@@ -2,29 +2,30 @@ import {Injectable} from '@nestjs/common';
 import algoliasearch, {SearchClient, SearchIndex} from "algoliasearch";
 import {MovieDto} from "../movies/dto/movie.dto";
 import {UpdateMovieDto} from "../movies/dto/update-movie.dto";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class AlgoliaService {
     private client: SearchClient;
     private index: SearchIndex;
 
-    constructor() {
-        this.client = algoliasearch('XXXX', 'XXXXX');
+    constructor(private configService: ConfigService) {
+        this.client = algoliasearch(configService.get<string>('ALGOLIA_APP_ID'), configService.get<string>('ALGOLIA_SECRET_KEY'));
         this.index = this.client.initIndex("Movies");
     }
 
-    async createNewMovie(movie: MovieDto) {
+    createNewMovie(movie: MovieDto) {
         return this.index.saveObject(movie).wait();
     }
 
-    async updateMovie(objectID: string, movie: UpdateMovieDto) {
+    updateMovie(objectID: string, movie: UpdateMovieDto) {
         return this.index.partialUpdateObject({
             objectID,
             ...movie
         }).wait();
     }
 
-    async deleteMovie(objectId: string) {
+    deleteMovie(objectId: string) {
         return this.index.deleteObject(objectId).wait();
     }
 }
