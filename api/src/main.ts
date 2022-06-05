b80796019cc9fca5bcc1c9949210cc2917a890a0
momo3038 <morgan.leroi@gmail.com>
2022-06-05 16:22:27 +0200

    ci(docker): Setup docker compose for API and APP

--- api/src/main.ts
+++ api/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 3001 );
 }
 
 
