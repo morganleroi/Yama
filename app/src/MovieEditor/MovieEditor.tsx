1145318559b1b424fcb8eae9fda8c817ebb4c3eb
momo3038 <morgan.leroi@gmail.com>
2022-06-07 22:15:50 +0200

    style: Add disable state management

--- app/src/MovieEditor/MovieEditor.tsx
+++ app/src/MovieEditor/MovieEditor.tsx
@@ -4,6 +4,7 @@ import 'antd/dist/antd.css';
 import {MovieDto} from '../MovieDto';
 import {MovieEditorError} from './MovieEditorError';
 import {MovieEditorSuccess} from './MovieEditorSuccess';
+import LoadingSpinner from "./Spinner";
 
 type FormState = 'KO' | 'OK';
 export type FormAction = 'CREATE' | 'UPDATE' | 'DELETE';
@@ -12,7 +13,12 @@ const MovieEditor = (props: { movie?: MovieDto; onClose: () => void, onCreateOrU
     const [formState, setFormState] = useState<FormState>();
     const [error, setError] = useState<string>();
     const [action, setAction] = useState<FormAction>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [form] = Form.useForm();
+
+    const addDelay = (ms: number) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 
     useEffect(() => {
         form.setFieldsValue(props.movie);
@@ -23,26 +29,33 @@ const MovieEditor = (props: { movie?: MovieDto; onClose: () => void, onCreateOrU
     };
 
     const onFinish = (values: any) => {
+        setFormState(undefined);
+        setIsLoading(true);
         setAction(props.movie?.objectID === undefined ? 'CREATE' : 'UPDATE');
         props.onCreateOrUpdate({
             ...props.movie,
             ...values,
         })
+            .then(() => addDelay(2000))
             .then(() => setFormState('OK'))
             .catch((err) => {
                 setFormState('KO');
                 setError(err.message);
-            });
+            }).finally(() => setIsLoading(false));
     };
 
     const onDelete = () => {
+        setFormState(undefined);
+        setIsLoading(true);
         setAction("DELETE");
         props.onDelete(props.movie?.objectID!)
+            .then(() => addDelay(2000))
             .then(() => setFormState('OK'))
+            .then(() => onClose())
             .catch((err) => {
                 setFormState('KO');
                 setError(err.message);
-            });
+            }).finally(() => setIsLoading(false));
     };
 
     const stateResult = () => {
@@ -80,7 +93,9 @@ const MovieEditor = (props: { movie?: MovieDto; onClose: () => void, onCreateOrU
                                     },
                                 ]}
                             >
-                                <Input placeholder="Please enter the official title"/>
+                                <Input disabled={isLoading}
+                                       className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                                       placeholder="Please enter the official title"/>
                             </Form.Item>
                         </Col>
                         <Col span={12}>
@@ -95,9 +110,9 @@ const MovieEditor = (props: { movie?: MovieDto; onClose: () => void, onCreateOrU
                                 ]}
                             >
                                 <Input
-                                    style={{
-                                        width: '100%',
-                                    }}
+                                    disabled={isLoading}
+                                    className="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                                    style={{width: '100%'}}
                                     placeholder="Please enter year"
                                 />
                             </Form.Item>
@@ -105,26 +120,30 @@ const MovieEditor = (props: { movie?: MovieDto; onClose: () => void, onCreateOrU
                     </Row>
                     <Row>
                         <Form.Item>
-                            <button
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    form.submit();
-                                }}
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            >
-                                {props.movie?.objectID ? 'Update the movie' : 'Create a new movie'}
-                            </button>
-                            {props.movie?.objectID && (
-                                <button
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        onDelete();
-                                    }}
-                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 ml-2 rounded"
+                            <Row className="items-center">
+                                <button disabled={isLoading}
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            form.submit();
+                                        }}
+                                        className="bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                                 >
-                                    Delete
+                                    {props.movie?.objectID ? 'Update the movie' : 'Create a new movie'}
                                 </button>
-                            )}
+                                {props.movie?.objectID && (
+                                    <button disabled={isLoading}
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                onDelete();
+                                            }}
+                                            className="bg-red-500 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-red-700 text-white font-bold py-2 px-4 ml-2 rounded"
+                                    >
+                                        Delete
+                                    </button>
+                                )}
+
+                                {isLoading && <LoadingSpinner/>}
+                            </Row>
                         </Form.Item>
                     </Row>
                 </Form>
---
+++ app/src/MovieEditor/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./spinner.css";
+
+export default function LoadingSpinner() {
+    return (
+        <div className="spinner-container ml-2">
+        <div className="loading-spinner">
+            </div>
+            </div>
+    );
+}
